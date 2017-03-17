9bf345ca6b2d60454c9fc0de8a9d22dd31e7c4a6
Robert Schwarz <mail@rschwarz.net>
2017-03-17 16:42:39 +0100

    update version to 0.4.0

--- src/csip.c
+++ src/csip.c
@@ -6,8 +6,8 @@
 #include "scip/scipdefplugins.h"
 
 #define CSIP_MAJOR_VERSION 0
-#define CSIP_MINOR_VERSION 3
-#define CSIP_PATCH_VERSION 6
+#define CSIP_MINOR_VERSION 4
+#define CSIP_PATCH_VERSION 0
 
 /* objective type */
 typedef int CSIP_OBJTYPE;
